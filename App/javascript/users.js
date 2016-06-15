2fa140fe7b23dc538500904459061f6cdff1e046
Lillojohn <j.emanuels@outlook.com>
2016-06-15 15:59:16 +0200

    Final merge registration

--- App/javascript/users.js
+++ App/javascript/users.js
@@ -45,6 +45,7 @@ function createUser(e) {
 function userSuccess(){
     $('#notice p').text("Bedankt voor het registreren!");
     TweenLite.to($('#notice p'), 0.2, {color: "#AFFF7E",  ease: Power2.easeOut});
+    TweenLite.to($('.button'), 0.2, {background: "rgba(34, 58, 58, 0.18)",  ease: Power2.easeOut});
     TweenLite.from($('#notice p'), 0.2, {opacity: 0,  ease: Power2.easeOut});
     registered = true;
 }
