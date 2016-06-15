7643d68770a6d9a940569ef22c36028345965b76
Lillojohn <j.emanuels@outlook.com>
2016-06-15 15:34:30 +0200

    Final register feature

--- App/javascript/navigation.js
+++ App/javascript/navigation.js
@@ -13,10 +13,10 @@ var menuOpen = false;
 function toggleMenu(){
     if(menuOpen == false)
     {
-        TweenLite.to($('header ul'), 0.2, {height: "25vh",  ease: Power2.easeOut})
+        TweenLite.to($('header ul'), 0.2, {height: "25vh",  ease: Power2.easeOut});
         menuOpen = true;
     } else {
-        TweenLite.to($('header ul'), 0.2, {height: "0",  ease: Power2.easeOut})
+        TweenLite.to($('header ul'), 0.2, {height: "0",  ease: Power2.easeOut});
         menuOpen = false;
     }
 }
@@ -27,7 +27,7 @@ function closeMenu(e){
         && $(e.target).context.tagName != "BUTTON"
         && $(e.target).context.tagName != "LI"
         && $(e.target).context.tagName != "A"){
-        TweenLite.to($('header ul'), 0.2, {height: "0",  ease: Power2.easeOut})
+        TweenLite.to($('header ul'), 0.2, {height: "0",  ease: Power2.easeOut});
         menuOpen = false;
     }
 }
--- App/javascript/users.js
+++ App/javascript/users.js
@@ -7,27 +7,45 @@
 
 })();
 
+var registered = false;
+
 function createUser(e) {
     e.preventDefault();
     var name = $('#name').val();
     var email = $('#email').val();
     var password = $('#password').val();
 
-    if( (name != null && name != "") &&
-        (email != null && email != "") &&
-        (password != null && password != "")){
+    console.log(registered);
 
-        $.ajax({
-            url: "http://timfalken.com/hr/annualnotes/users",
-            method: "POST",
-            dataType: "json",
-            data: '{"name": "'+ name +'","email":"'+ email +'","password":"'+ password +'"}'
-        })
+    if(registered == false ){
+        if( (name != null && name != "") &&
+            (email != null && email != "") &&
+            (password != null && password != "")){
 
+            $.ajax({
+                url: "http://timfalken.com/hr/annualnotes/users",
+                method: "POST",
+                dataType: "json",
+                data: '{"name": "'+ name +'","email":"'+ email +'","password":"'+ password +'"}',
+                complete: userSuccess
+            })
+
+        } else {
+            $('#notice p').text("Niet alles is goed ingevuld");
+            TweenLite.from($('#notice p'), 0.2, {opacity: 0,  ease: Power2.easeOut});
+        }
     } else {
-
+        $('#notice p').text("Je hebt al een account gemaakt");
+        TweenLite.to($('#notice p'), 0.2, {color: "#FFABDA",  ease: Power2.easeOut});
+        TweenLite.from($('#notice p'), 0.2, {opacity: 0,  ease: Power2.easeOut});
     }
 
+}
 
+function userSuccess(){
+    $('#notice p').text("Bedankt voor het registreren!");
+    TweenLite.to($('#notice p'), 0.2, {color: "#AFFF7E",  ease: Power2.easeOut});
+    TweenLite.from($('#notice p'), 0.2, {opacity: 0,  ease: Power2.easeOut});
+    registered = true;
 }
 
