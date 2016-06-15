b8e9b2edea787ebb86142c4619cc89eec5f71eb4
Lillojohn <j.emanuels@outlook.com>
2016-06-15 14:28:47 +0200

    Create user getest

--- App/javascript/users.js
+++ App/javascript/users.js
@@ -4,7 +4,7 @@ function createUser() {
         url: "http://timfalken.com/hr/annualnotes/users",
         method: "POST",
         dataType: "json",
-        data: '{"name":"John","email":"johnyemanuels@hotmail.nl","password":"132456"}',
+        data: '{"name":"John","email":"johnyemanuels@hotmailz.nl","password":"132456"}',
         complete: function(data){
             console.log(data)
         }
