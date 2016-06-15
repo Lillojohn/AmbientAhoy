2ce204c636ff3ba1e5edbe90f3929037140ffd9c
Lillojohn <j.emanuels@outlook.com>
2016-06-15 08:37:07 +0200

    scriptjs

--- App/javascript/script.js
+++ App/javascript/script.js
@@ -29,6 +29,19 @@ function createNote(){
     })
 }
 
+function createEvent(){
+    $.ajax({
+        "title" : "test",
+        "problem" : "need to test POST",
+        "solution" : "this, hopefully",
+        "imageUrls" : [],
+        "eventId" : 1,
+        "userId" : 1,
+        "userKey" : "202cb962ac59075b964b07152d234b70"
+    })
+}
+
+
 function addClickHandler(){
     $('.event').on("click", clickAnimation);
 }
